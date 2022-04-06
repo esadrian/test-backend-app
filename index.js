f8e3246a1bf694872205c567c724ab2ede7bda33
Diego Acuña <diegoacunaberger@gmail.com>
2022-04-06 18:34:12 +0200

    historical countries last year

--- index.js
+++ index.js
@@ -20,3 +20,34 @@ app.get('/api/nationalize/:name', async (req, res) => {
   const response = await axios.get(`https://api.nationalize.io/?name=${name}`);
   res.json(response.data);
 });
+
+app.get('/api/agify/:name', async (req, res) => {
+  const { name } = req.params;
+  const response = await axios.get(`https://api.agify.io/?name=${name}`);
+  res.json(response.data);
+});
+
+// app.get('/api/covid', async (req, res) => {
+//   const response = await axios.get(
+//     'https://corona.lmao.ninja/v2/continents?yesterday=true&sort'
+//   );
+//   res.json(response.data);
+// });
+
+app.get('/api/covid/:countries', async (req, res) => {
+  const { countries } = req.params;
+  const response = await axios.get(
+    `https://corona.lmao.ninja/v2/countries/${countries}?yesterday=true&strict=true&query
+    `
+  );
+  res.json(response.data);
+});
+
+app.get('/api/covid/historical/:countries', async (req, res) => {
+  const { countries } = req.params;
+  const response = await axios.get(
+    `https://corona.lmao.ninja/v2/historical/${countries}?lastdays=365
+      `
+  );
+  res.json(response.data);
+});
